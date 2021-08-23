468003e76949779cd1e0c2af198bc777f0af2752
matias685 <78141961+matias685@users.noreply.github.com>
2021-08-23 14:20:40 +0300

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -4,26 +4,31 @@
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 10;       /* snap pixel */
+
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true", "FontAwesome:style=Regular:size=12:antialias=true:autohint=true", "Noto Color Emoji:style=Regular:pixelsize18:antialias=true:autohint=true" };
+
+static const char *fonts[]          = { "Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true", "Noto Color Emoji" "FontAwesome:style=Regular:size=12:antialias=true:autohint=true", "Noto Color Emoji:style=Regular:pixelsize18:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true";
-static const char col_gray1[]       = "#181818";
-static const char col_gray2[]       = "#303030";
-static const char col_gray3[]       = "#c5c8c6";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#50C878"; 
+
+static const char  col_white[] = "#f8f8f2";
+static const char  col_gray1[] = "#282a36";
+static const char  col_gray2[] = "#44475a";
+static const char  col_black[] = "#21222C";
+static const char  col_com[]   = "#6272A4";
+static const char  col_pink[]  = "#ff79c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm] = { col_white, col_gray1, col_gray1 }, // client's default
+    [SchemeSel]  = { col_white, col_gray2, col_pink },  // for selected client
 };
 
 /* tagging */
@@ -66,9 +71,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_pink, "-sf", col_white, NULL };
+
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "brave", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 static const char *filemanagercmd[] = { "pcmanfm", NULL };
 
 static Key keys[] = {
