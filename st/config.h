99408fd76e9b867cb74c8e2b95865a2a734e2182
matias685 <78141961+matias685@users.noreply.github.com>
2022-01-29 02:55:07 +0200

    Add files via upload

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Iosevka:style=Regular:pixelsize=18";
 /* Spare fonts */
 static char *font2[] = {
 	"FontAwesome:style=Regular:pixelsize=18:antialias=true:autohint=true", // Powerline 
@@ -197,7 +197,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -205,16 +205,20 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_K,       	zoom,           {.f = +1} },
+	{ TERMMOD,              XK_J,        	zoom,           {.f = -1} },
+	{ TERMMOD,              XK_U,       	zoom,           {.f = +2} },
+	{ TERMMOD,              XK_D,        	zoom,           {.f = -2} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,           	XK_k,     	kscrollup,      {.i = 1} },
+	{ MODKEY,               XK_j,   	kscrolldown,    {.i = 1} },
+	{ MODKEY,           	XK_u,     	kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_d,   	kscrolldown,    {.i = -1} },
 };
 
 /*
