36c0fd6ac7b9d732b0400df335b38e6f9c945c5f
matias685 <78141961+matias685@users.noreply.github.com>
2022-10-10 14:30:49 +0300

    Add files via upload

---
+++ dwm/colors/onedark.h
@@ -0,0 +1,27 @@
+static const char col_gray1[]       = "#282C34";
+static const char col_gray2[]       = "#ABB2BF";
+static const char col_gray3[]       = "#ABB2BF";
+static const char col_gray4[]       = "#282C34";
+static const char col_gray5[]       = "#c678DD";
+static const char selbordercolor[]  = "#cc241d";
+static const char col_cyan[]        = "#E06C75";
+
+static const char black[]           = "#282C34";
+static const char white[]           = "#ABB2BF";
+static const char gray2[]           = "#4b5263";
+static const char gray3[]           = "#5c6370";
+static const char gray4[]           = "#282C34";
+static const char blue[]            = "#61afef";
+static const char cyan[]            = "#56b6c2";
+static const char green[]           = "#98c379";
+static const char red[]             = "#b35046";
+static const char orange[]          = "#d19a66";
+static const char yellow[]          = "#e5c07b";
+static const char pink[]            = "#e06c75";
+static const char col_borderbar[]   = "#282C34";
+static const char *colors[][3]      = {
+	/*                    fg         bg       border   */
+	[SchemeNorm]      = { gray2,    black,    gray2 },
+	[SchemeSel]       = { gray2,    pink,     red  },
+};
+
--- dwm/colors/stock.h
+++ dwm/colors/stock.h
@@ -1,1 +1,18 @@
-
+static const char col_gray1[]       = "#181818";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_white[]       = "#ffffff";
+static const char col_cyan[]        = "#005577";
+static const char col_cyan2[]       = "#2a52be";
+static const char col_blue[]        = "#0D6A9B";
+static const char col_red[]	        = "#540c00";
+static const char col_red2[]	    = "#770000";
+static const char red3[]            = "#b35046";
+static const char col_yellow[]	    = "#dfca81";
+static const char selbordercolor[]  = "#cc241d";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_white, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_white, col_blue,  col_blue },
+};
